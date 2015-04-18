866b403a62187fc5fd3c5d991ba31f185477f198
ktvexe <ktvexe@gmail.com>
2015-04-18 09:42:51 +0800

    sudoku header

---
+++ Sudoku.h
@@ -0,0 +1,41 @@
+#ifndef SUDOKU_H
+#define SUDOJU_H
+
+#include <iostream>
+#include<fstream>
+#include<vector>
+#include<cstdlib>
+#include<algorithm>
+
+using namespace std;
+
+typedef struct{
+	unsigned int contain: 9;
+	unsigned int:0;
+}Pixel;
+
+class Sudoku{
+	public:
+		Sudoku(void);
+		void ReadIn(void);
+		void Solve(void);
+		void GiveQuestion(void);
+
+	private:
+		static const int Map[15][9][9];
+		int initialCheckUnity(void);
+		int getCountSolution(void);
+		void getAnswer(void);
+		void outOriginMap(void);
+		void outAnswer(void);
+		int solveOne(vector<int> Test, vector<Pixel> X, vector<Pixel> Y, vector<Pixel> G);
+		int solveMulti(vector<int> Test, vector<Pixel> X, vector<Pixel> Y, vector<Pixel> G);
+
+		vector<Pixel> xLable;
+		vector<Pixel> yLable;
+		vector<Pixel> gridLable;
+		vector<int> map;
+		vector< vector<int> > multiAnswer;
+
+
+#endif
